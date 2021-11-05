6c9b5810b0510cfff16226078788d08ef18d943c
Agustinf90 <agustinf_tw@hotmail.com>
2021-11-05 12:25:33 -0300

    2d

---
+++ src/components/navBar.js
@@ -0,0 +1,17 @@
+
+export const NavBar = () => {
+
+
+    return <header>
+       
+    <h1>Logo</h1>
+    <nav>
+    <ul>
+    <li> </li>
+    <li> </li>
+    <li> </li>
+         </ul>
+    </nav>
+    </header>
+}
+
